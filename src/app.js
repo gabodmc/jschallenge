90bba03be838b4f294d785356bc8da8158906b32
gabodmc <gabrielmc@live.com.ar>
2021-09-22 12:06:20 -0300

    Minor correction in model/concept.js

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const methodOverride = require ('method-override')
+const cors = require('cors')
+
+const app = express();
+const path = require('path');
+const port = process.env.PORT;
+
+const homeRouter = require('./routes/homeRouter');
+const apiMovementsRouter = require('./routes/api/apiMovementRouter');
+
+app.set('views', path.resolve(__dirname, './views'));
+app.set ('view engine', 'ejs');
+
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(cors());
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+
+
+app.use('/', homeRouter);
+app.use('/api/movements',apiMovementsRouter);
+
+
+app.listen (port || 3001, () => {
+    console.log('Servidor levantado en el puerto 3001');
+});
--- src/database/models/concept.js
+++ src/database/models/concept.js
@@ -10,7 +10,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+    Concept.hasMany(models.Transaction, {
+      foreignKey: 'conceptId',
+      as: 'transaction'
+    })
     }
   };
   Concept.init({
