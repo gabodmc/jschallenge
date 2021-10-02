cb03b8743e147ac5406cf33b5ad55d4b4c5a1a86
gabodmc <gabrielmc@live.com.ar>
2021-10-02 12:24:54 -0300

    Adding validations to Edit Route

--- backend/src/controllers/api/apiTransactionController.js
+++ backend/src/controllers/api/apiTransactionController.js
@@ -61,20 +61,26 @@ const apiTransactionController = {
             }
         
     } else {
-        console.log({errors:errors.mapped()})
+        res.json({errors:errors.mapped()})
     }
     },
 
     update: (req, res) => {
         let updatedTransaction = req.body
-
+        let errors = validationResult(req);
+        if (errors.isEmpty()) {
+            try {
         Transaction.update(updatedTransaction,
             {
                 where: {
                     id: req.params.id
                 }
             }).then((result) => res.json(result));
-    },
+        } catch (error){
+            res.json(error)
+        }
+            
+    }},
 
     delete: (req, res) => {
         Transaction.destroy({
--- backend/src/middlewares/validations.js
+++ backend/src/middlewares/validations.js
@@ -12,10 +12,22 @@ const validateTransaction = [
         .notEmpty().withMessage('El campo de fecha es obligatorio')
         .bail().isDate().withMessage('El campo debe ser en formato fecha'),
     body('revenue')
-        .notEmpty().withMessage('Recordá ingresar una contraseña'),
+        .notEmpty().withMessage('Debes seleccionar un tipo de operación'),
+];
+
+const editTransaction = [
+    body('concept')
+        .notEmpty().withMessage('Recordá ingresar concepto').bail()
+        .isLength({ min: 2 }).withMessage('El concepto debe tener mas de dos caracteres'),
+    body('amount')
+        .notEmpty().withMessage('El campo de monto es obligatorio')
+        .bail().isNumeric().withMessage('El monto debe ser numérico'),
+    body('date')
+        .notEmpty().withMessage('El campo de fecha es obligatorio')
+        .bail().isDate().withMessage('El campo debe ser en formato fecha'),
 ];
 
 
 
 
-module.exports = { validateTransaction }
+module.exports = { validateTransaction, editTransaction }
